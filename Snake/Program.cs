21d7d8bfe85fa883610931bb01d7f94defaf4121
Arthur <iamegoiste@gmail.com>
2020-06-04 23:54:15 +0300

    Lesson 9

--- Snake/Program.cs
+++ Snake/Program.cs
@@ -10,10 +10,6 @@ namespace Snake
     {
         static void Main(string[] args)
         {
-
-            Point p = new Point(4, 5, '*');
-            p.Draw();
-
             HorizontalLine upLine = new HorizontalLine(0, 78, 0, '+');
             HorizontalLine downLine = new HorizontalLine(0, 78, 24, '+');
             VerticalLine leftLine = new VerticalLine(0, 24, 0, '+');
@@ -23,6 +19,10 @@ namespace Snake
             downLine.Draw();
             leftLine.Draw();
             rightLine.Draw();
+
+            Point p = new Point(4, 5, '*');
+            Snake snake = new Snake(p, 4, Direction.RIGHT);
+            snake.Draw();
         }
     }
 }
