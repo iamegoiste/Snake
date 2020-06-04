0a1d55c0ce77b8e17d429112afb66ef166b6851c
Arthur <iamegoiste@gmail.com>
2020-06-04 21:36:58 +0300

    Lesson7-Horizontal Line

--- Snake/Program.cs
+++ Snake/Program.cs
@@ -1,5 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 
 namespace Snake
 {
@@ -13,31 +16,8 @@ namespace Snake
             Point p2 = new Point(4, 5, '#');
             p2.Draw();
 
-            Point p3 = new Point(6, 8, '$');
-            p3.Draw();
-
-            Point p4 = new Point(6, 10, '@');
-            p4.Draw();
-
-            List<int> numList = new List<int>();
-            numList.Add(0);
-            numList.Add(1);
-            numList.Add(2);
-
-            int x = numList[0];
-            int y = numList[1];
-            int z = numList[2];
-
-            //foreach(int i in numList)
-            //{
-            //    Console.WriteLine(i);
-            //}
-
-            List<Point> pList = new List<Point>();
-            pList.Add(p1);
-            pList.Add(p2);
-            pList.Add(p3);
-            pList.Add(p4);
+            HorizontalLine line = new HorizontalLine(5, 10, 8, '+');
+            line.Draw();
         }
     }
 }
