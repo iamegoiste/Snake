9f7a9d3270191450442082cc184d749c08777692
Arthur <iamegoiste@gmail.com>
2020-06-05 13:05:57 +0300

    Lesson 12
    Snake Eat

--- Snake/Point.cs
+++ Snake/Point.cs
@@ -47,6 +47,11 @@ namespace Snake
             }
         }
 
+        public bool IsHit(Point p)
+        {
+            return p.x == this.x && p.y == this.y;
+        }
+
         internal void Clear()
         {
             sym = ' ';
